414240a32feafa1cea219d1d082fafad069219a2
Ray Dino <ray.e.dino@gmail.com>
2023-04-08 21:10:53 +0200

    Allows player to make guesses

--- src/lib.rs
+++ src/lib.rs
@@ -6,34 +6,81 @@ pub mod setup {
     use std::path::Path;
 
     pub struct SecretWord {
-        pub word: String,
+        word: String,
     }
 
     impl SecretWord {
-        pub fn new() -> SecretWord {
-            let mut words_file = get_project_root().expect("Can't file project root.");
-            words_file.push("data/words.txt");
+        pub fn new() -> Self {
+            const WORDS_PATH : &str = "data/words.txt";
+            let project_root = get_project_root().expect("Can't find project root.");
+            let words_file = project_root.join(WORDS_PATH);
     
             let lines = SecretWord::read_lines(words_file);
     
             let word_index = rand::thread_rng().gen_range(0..lines.len());
-            SecretWord {
+            Self {
                 word: lines[word_index].clone()
             }
         }
     
         fn read_lines(filename: impl AsRef<Path>) -> Vec<String> {
-            let file = File::open(filename).expect("no such file");
+            // TODO: Return Result instead. Handle error in caller.
+            let file = File::open(filename).expect("No such file");
             let buf = BufReader::new(file);
             buf.lines()
                 .map(|l| l.expect("Could not parse line"))
                 .collect()
+        }
+
+        pub fn word(&self) -> &String {
+            &self.word
         }
     }
 
 
 }
 
-mod game {
+pub mod game {
+    use std::io;
 
+    use crate::setup::SecretWord;
+
+    const TURNS: u8 = 5;
+
+    pub struct Guess {
+        pub guess: String
+    }
+
+    impl Guess {
+        pub fn make_a_guess() -> io::Result<Self> {
+            let mut guess = String::new();
+            io::stdin().read_line(&mut guess)?;
+
+            Ok(Guess { guess: guess })
+        }
+    }
+
+    pub struct Game {
+        secret_word: SecretWord,
+        turns: u8
+    }
+
+    impl Game {
+        pub fn new(secret_word: SecretWord) -> Self {
+            dbg!(secret_word.word());
+            Self {
+                secret_word: secret_word,
+                turns: TURNS
+            }
+        }
+
+        fn take_a_guess() {
+
+        }
+
+        pub fn get_turns(&self) -> u8 {
+            self.turns
+        }
+        
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+
+
 use wordle::setup::SecretWord;
+use wordle::game::Game;
+use wordle::game::Guess;
 
 fn main() {
     run();
 }
 
 fn run() {
-    let secret_word = SecretWord::new();
-    println!("{}", secret_word.word);
+    let game = Game::new(SecretWord::new());
+    for _turn in 0..game.get_turns() {
+        let guess = Guess::make_a_guess().expect("Invalid guess");
+        dbg!(guess.guess);
+    }
+
 }
+
+
 
 
